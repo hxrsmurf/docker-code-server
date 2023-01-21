17c81b46b5d68311c19990e9cef7da418874df0b
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-21 20:04:10 +0000

    Add docker_image var to coder

--- templates/coder/variables.tf
+++ templates/coder/variables.tf
@@ -27,3 +27,21 @@ variable "repo" {
     error_message = "Invalid repo!"
   }
 }
+
+variable "docker_image" {
+  description = "What Docker image would you like to use for your workspace?"
+  default     = "base"
+
+  # List of images available for the user to choose from.
+  # Delete this condition to give users free text input.
+  validation {
+    condition     = contains(["base"], var.docker_image)
+    error_message = "Invalid Docker image!"
+  }
+
+  # Prevents admin errors when the image is not found
+  validation {
+    condition     = fileexists("images/${var.docker_image}.Dockerfile")
+    error_message = "Invalid Docker image. The file does not exist in the images directory."
+  }
+}
