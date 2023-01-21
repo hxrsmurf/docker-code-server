36ce15b5d0d39f623944c9be9f7c240669a8b40b
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-21 19:57:58 +0000

    Update coder

--- templates/coder/main.tf
+++ templates/coder/main.tf
@@ -68,50 +68,6 @@ resource "coder_app" "code-server" {
 
 }
 
-variable "name" {
-  description = "What name should be used for Git?"
-  default = "First Last"
-}
-
-variable "email" {
-  description = "What name should be used for Git?"
-  default = "First.Last@domain.com"
-}
-
-variable "username" {
-  description = "What is your GitHub username?"
-  default = "user"
-}
-
-variable "repo" {
-  description = "What repo to clone?"
-  default = "docker-code-server"
-  validation {
-    condition = contains([
-      "docker-code-server"
-    ], var.repo)
-    error_message = "Invalid repo!"
-  }
-}
-
-variable "docker_image" {
-  description = "What Docker image would you like to use for your workspace?"
-  default     = "base"
-
-  # List of images available for the user to choose from.
-  # Delete this condition to give users free text input.
-  validation {
-    condition     = contains(["base"], var.docker_image)
-    error_message = "Invalid Docker image!"
-  }
-
-  # Prevents admin errors when the image is not found
-  validation {
-    condition     = fileexists("images/${var.docker_image}.Dockerfile")
-    error_message = "Invalid Docker image. The file does not exist in the images directory."
-  }
-}
-
 resource "docker_volume" "home_volume" {
   name = "coder-${data.coder_workspace.me.id}-home"
   # Protect the volume from being deleted due to changes in attributes.
---
+++ templates/coder/variables.tf
@@ -0,0 +1,25 @@
+variable "name" {
+  description = "What name should be used for Git?"
+  default = "First Last"
+}
+
+variable "email" {
+  description = "What name should be used for Git?"
+  default = "First.Last@domain.com"
+}
+
+variable "username" {
+  description = "What is your GitHub username?"
+  default = "user"
+}
+
+variable "repo" {
+  description = "What repo to clone?"
+  default = "docker-code-server"
+  validation {
+    condition = contains([
+      "docker-code-server"
+    ], var.repo)
+    error_message = "Invalid repo!"
+  }
+}
