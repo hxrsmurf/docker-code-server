e0da7884f1b791b92184dc7e7335f4db67a387ff
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-21 14:48:50 +0000

    Add main.tf

---
+++ templates/coder/main.tf
@@ -0,0 +1,203 @@
+terraform {
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = "0.6.6"
+    }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.20.2"
+    }
+  }
+}
+
+data "coder_provisioner" "me" {
+}
+
+provider "docker" {
+}
+
+data "coder_workspace" "me" {
+}
+
+resource "coder_agent" "main" {
+  arch           = data.coder_provisioner.me.arch
+  os             = "linux"
+  env = {
+    GIT_AUTHOR_NAME = "${var.name}"
+    GIT_COMMITTER_NAME = "${var.name}"
+    GIT_AUTHOR_EMAIL = "${var.email}"
+    GIT_COMMITTER_EMAIL = "${var.email}"
+  }
+  startup_script = <<EOT
+    #!/bin/bash
+
+    mkdir -p /home/coder/repos
+
+    cd /home/coder/repos && git clone https://github.com/${var.username}/${var.repo}
+
+    # install and start code-server
+    curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 4.8.3 | tee code-server-install.log
+    code-server --install-extension GitHub.vscode-pull-request-github
+    code-server --install-extension vscodevim.vim
+    code-server --install-extension dsznajder.es7-react-js-snippets
+    code-server --install-extension hashicorp.terraform
+    code-server --install-extension ms-python.python
+    code-server --install-extension bradlc.vscode-tailwindcss
+    code-server --install-extension esbenp.prettier-vscode
+    code-server --uninstall-extension ms-toolsai.jupyter
+    code-server --uninstall-extension ms-python.isort
+    code-server --auth none --port 13337 | tee code-server-install.log &
+  EOT
+}
+
+resource "coder_app" "code-server" {
+  agent_id     = coder_agent.main.id
+  slug         = "code-server"
+  display_name = "code-server"
+  url          = "http://localhost:13337/?folder=/home/coder/repos"
+  icon         = "/icon/code.svg"
+  subdomain    = false
+  share        = "owner"
+
+  healthcheck {
+    url       = "http://localhost:13337/healthz"
+    interval  = 3
+    threshold = 10
+  }
+
+}
+
+variable "name" {
+  description = "What name should be used for Git?"
+  default = "First Last"
+}
+
+variable "email" {
+  description = "What name should be used for Git?"
+  default = "First.Last@domain.com"
+}
+
+variable "username" {
+  description = "What is your GitHub username?"
+  default = "user"
+}
+
+variable "repo" {
+  description = "What repo to clone?"
+  default = "docker-code-server"
+  validation {
+    condition = contains([
+      "docker-code-server"
+    ], var.repo)
+    error_message = "Invalid repo!"
+  }
+}
+
+variable "docker_image" {
+  description = "What Docker image would you like to use for your workspace?"
+  default     = "base"
+
+  # List of images available for the user to choose from.
+  # Delete this condition to give users free text input.
+  validation {
+    condition     = contains(["base"], var.docker_image)
+    error_message = "Invalid Docker image!"
+  }
+
+  # Prevents admin errors when the image is not found
+  validation {
+    condition     = fileexists("images/${var.docker_image}.Dockerfile")
+    error_message = "Invalid Docker image. The file does not exist in the images directory."
+  }
+}
+
+resource "docker_volume" "home_volume" {
+  name = "coder-${data.coder_workspace.me.id}-home"
+  # Protect the volume from being deleted due to changes in attributes.
+  lifecycle {
+    ignore_changes = all
+  }
+  # Add labels in Docker to keep track of orphan resources.
+  labels {
+    label = "coder.owner"
+    value = data.coder_workspace.me.owner
+  }
+  labels {
+    label = "coder.owner_id"
+    value = data.coder_workspace.me.owner_id
+  }
+  labels {
+    label = "coder.workspace_id"
+    value = data.coder_workspace.me.id
+  }
+  # This field becomes outdated if the workspace is renamed but can
+  # be useful for debugging or cleaning out dangling volumes.
+  labels {
+    label = "coder.workspace_name_at_creation"
+    value = data.coder_workspace.me.name
+  }
+}
+
+resource "docker_image" "coder_image" {
+  name = "coder-base-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
+  build {
+    path       = "./images/"
+    dockerfile = "${var.docker_image}.Dockerfile"
+    tag        = ["coder-${var.docker_image}:v0.1"]
+  }
+
+  # Keep alive for other workspaces to use upon deletion
+  keep_locally = false
+}
+
+resource "docker_container" "workspace" {
+  count = data.coder_workspace.me.start_count
+  image = docker_image.coder_image.latest
+  # Uses lower() to avoid Docker restriction on container names.
+  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
+  # Hostname makes the shell more user friendly: coder@my-workspace:~$
+  hostname = data.coder_workspace.me.name
+  # Use the docker gateway if the access URL is 127.0.0.1
+  entrypoint = ["sh", "-c", replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")]
+  env        = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
+
+  dns = ["192.168.1.1"]
+
+  host {
+    host = "host.docker.internal"
+    ip   = "host-gateway"
+  }
+  volumes {
+    container_path = "/home/coder/"
+    volume_name    = docker_volume.home_volume.name
+    read_only      = false
+  }
+  # Add labels in Docker to keep track of orphan resources.
+  labels {
+    label = "coder.owner"
+    value = data.coder_workspace.me.owner
+  }
+  labels {
+    label = "coder.owner_id"
+    value = data.coder_workspace.me.owner_id
+  }
+  labels {
+    label = "coder.workspace_id"
+    value = data.coder_workspace.me.id
+  }
+  labels {
+    label = "coder.workspace_name"
+    value = data.coder_workspace.me.name
+  }
+}
+
+resource "coder_metadata" "container_info" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_container.workspace[0].id
+
+  item {
+    key   = "image"
+    value = var.docker_image
+  }
+}
