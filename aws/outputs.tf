2048c6540a88616ae1cdaa2e32a7ae9cc3b87f5a
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-22 05:41:46 +0000

    Add outputs.tf

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -18,3 +18,12 @@ output "user" {
     "url" = "https://us-east-1.console.aws.amazon.com/iamv2/home?region=us-east-1#/users/details/${aws_iam_user.user.name}"
   }
 }
+
+resource "local_file" "credentials" {
+  filename = "credentials"
+  content = <<EOT
+  [default]
+  aws_access_key_id = ${aws_iam_access_key.key.id}
+  aws_secret_access_key = ${aws_iam_access_key.key.secret}
+  EOT
+}
