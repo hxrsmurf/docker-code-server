1a764696a1860d0ad6b428fff91379ee937547a7
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-22 06:11:41 +0000

    Create inline-policy

--- aws/user.tf
+++ aws/user.tf
@@ -3,6 +3,12 @@ resource "aws_iam_user" "user" {
   path = "/"
 }
 
+resource "aws_iam_user_policy" "policy" {
+  name = data.aws_iam_policy_document.inline_policy.policy_id
+  policy = data.aws_iam_policy_document.inline_policy.json
+  user = aws_iam_user.user.name
+}
+
 resource "aws_iam_user_policy_attachment" "policy-attachment" {
   user       = aws_iam_user.user.name
   policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
