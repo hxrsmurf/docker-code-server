b90a50842ab4050cbbfd4a1f9d3a124ece3809d5
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-22 05:18:03 +0000

    Add access key and output

--- aws/user.tf
+++ aws/user.tf
@@ -8,6 +8,15 @@ resource "aws_iam_user_policy_attachment" "policy-attachment" {
   policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
 }
 
+resource "aws_iam_access_key" "key" {
+  user    = aws_iam_user.user.name
+}
+
+output "secret" {
+  value = aws_iam_access_key.key.secret
+  sensitive = true
+}
+
 output "user" {
   value = {
     "url" = "https://us-east-1.console.aws.amazon.com/iamv2/home?region=us-east-1#/users/details/${aws_iam_user.user.name}"
