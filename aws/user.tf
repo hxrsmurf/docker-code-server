1c3cf5c3ca167df83a5cf68cde81148c9125fa03
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-22 06:20:04 +0000

    Add ec2-inline-policy.tf

--- aws/user.tf
+++ aws/user.tf
@@ -9,6 +9,12 @@ resource "aws_iam_user_policy" "policy" {
   user = aws_iam_user.user.name
 }
 
+resource "aws_iam_user_policy" "ec2-policy" {
+  name = data.aws_iam_policy_document.ec2_inline_policy.policy_id
+  policy = data.aws_iam_policy_document.ec2_inline_policy.json
+  user = aws_iam_user.user.name
+}
+
 resource "aws_iam_user_policy_attachment" "policy-attachment" {
   user       = aws_iam_user.user.name
   policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
