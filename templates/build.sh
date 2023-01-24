c1f794a4b604864b15f3e7832f3b19341905389e
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-24 01:11:36 +0000

    Update build.sh to tag the image

--- templates/build.sh
+++ templates/build.sh
@@ -1,4 +1,4 @@
 sudo docker build -f coder/images/builder.Dockerfile . --tag builder
-sudo docker build -f coder/images/debian.Dockerfile .
-sudo docker build -f coder/images/node.Dockerfile .
-sudo docker build -f coder/images/python.Dockerfile .
+sudo docker build -f coder/images/debian.Dockerfile . --tag coder-debian:v0.1
+sudo docker build -f coder/images/node.Dockerfile . --tag coder-node:v0.1
+sudo docker build -f coder/images/python.Dockerfile . --tag coder-python:v0.1
